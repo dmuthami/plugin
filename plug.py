2aa328ff1d8617c49e3cf91869ae19778dcb663b
dmuthami@tuta.io <dmuthami@tuta.io>
2021-05-21 11:53:59 +0300

    Version 3

--- plug.py
+++ plug.py
@@ -7,6 +7,7 @@ county_lyr=QgsVectorLayer(county_fn,'County','ogr')
 
 #Extract centroid for each polygon
 features= county_lyr.getFeatures()
+pointsList = []
 for feature in features:
     geom= str(feature.geometry().centroid().asPoint())
     #get coordinates
@@ -18,12 +19,23 @@ for feature in features:
     x=cordArra[0]
     y=cordArra[1]
     # Create point feature
-    #feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(453257, 4631706)))
-    
-    print(x,y)
-    #print(cordArra)
+    point_feature=QgsGeometry.fromPointXY(QgsPointXY(float(x), float(y)))
+    pointsList.append(point_feature)
+    #print(x,y)
+print(pointsList)
 #Create a new point layer
+mem_vectorLyr =  QgsVectorLayer('Point?crs=epsg:4326&field=name:string(25)&code=population:integer', 'centroid_points' , "memory")
+prov = mem_vectorLyr.dataProvider()#get data provider
 
+root = QgsProject.instance() #get layer root
+feats = [ QgsFeature() for feature in range(len(pointsList)) ]
+
+for feature, feat in enumerate(feats):
+    feat.setAttributes([feature])
+    feat.setGeometry(featureList[feature])
+
+prov.addFeatures(feats) #Add points feature layer
+root.addMapLayer(mem_vectorLyr) # Add layer to root
 #Set symbology for the point layer
 
 #save the layer
